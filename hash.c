75e289def7df65370753b0ee1b1948bfd9dabbde
yuhonghuang07 <yunmenyeyu@gmail.com>
2018-10-22 22:11:06 -0300

    agregue funcion de hash -tony

--- hash.c
+++ hash.c
@@ -40,6 +40,17 @@ int hashing(char* cadena){
     return (hash & 0x7FFFFFFF);
 }
 
+int BKDRHash(char *str){
+ 	int seed = 131; // 31 131 1313 13131 131313 etc..
+	int hash = 0;
+
+	while (*str){
+		hash = hash * seed + (*str++);
+	}
+
+	return (hash & 0x7FFFFFFF);
+}
+
 /* *****************************************************************
  *                    PRIMITIVAS DEL HASH
  * *****************************************************************/
